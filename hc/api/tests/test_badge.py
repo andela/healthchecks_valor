6b23706aba5f13023fdc2b7d71e3b317c41d9aa8
andela-ik <ian.kingori@andela.com>
2017-01-19 18:01:29 +0300

    #137591921: Test badge: Bad Signature and SVG tests updated

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -14,6 +14,8 @@ class BadgeTestCase(BaseTestCase):
     def test_it_rejects_bad_signature(self):
         r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
         ### Assert the expected response status code
+        """ Bad signature should throw a Http Bad Request error"""
+        self.assertEqual(r.status_code, 400)
 
     def test_it_returns_svg(self):
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
@@ -22,3 +24,5 @@ class BadgeTestCase(BaseTestCase):
 
         r = self.client.get(url)
         ### Assert that the svg is returned
+        """ Check content type of the response received"""
+        self.assertEqual(r['Content-Type'], "image/svg+xml")
