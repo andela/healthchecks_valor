005120a6419224b9e8006d1808e1c82224ece44f
andela-hms <humphrey.musonye@andela.com>
2017-01-30 12:48:32 +0300

    [chore #137591919] Fixed add_pushbover test

--- hc/front/tests/test_add_pushbover.py
+++ hc/front/tests/test_add_pushbover.py
@@ -37,4 +37,15 @@ class AddPushoverTestCase(BaseTestCase):
         r = self.client.get("/integrations/add_pushover/?%s" % params)
         assert r.status_code == 403
 
-    ### Test that pushover validates priority
+    ### Test that pushover validates priority    
+    def test_it_validates_priority(self):
+        self.client.login(username="alice@example.org", password="password")
+
+        session = self.client.session
+        session["po_nonce"] = "n"
+        session.save()
+
+        # Assign the wrong priority parameter
+        params = "pushover_user_key=a&nonce=n&prio=123"
+        r = self.client.get("/integrations/add_pushover/?%s" % params)
+        assert r.status_code == 400
