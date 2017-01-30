26391334e3aed670f485e4a8fe6183d7eacfd7c1
bmuthoga <bmuthoga@gmail.com>
2017-01-30 22:06:00 +0300

    Added files to ch-accounts-tests-137591917 branch.

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from hc.test import BaseTestCase
+from .. import views
+import json 
 
 
 class CheckTokenTestCase(BaseTestCase):
@@ -8,6 +10,7 @@ class CheckTokenTestCase(BaseTestCase):
         super(CheckTokenTestCase, self).setUp()
         self.profile.token = make_password("secret-token")
         self.profile.save()
+        self.client.login(username="bob@example.org", password="password")
 
     def test_it_shows_form(self):
         r = self.client.get("/accounts/check_token/alice/secret-token/")
@@ -22,7 +25,20 @@ class CheckTokenTestCase(BaseTestCase):
         self.assertEqual(self.profile.token, "")
 
     ### Login and test it redirects already logged in
+    def test_login_and_redirects_already_logged_in(self):
+        # res = self.client.post(email="bob@example.org", password="password")
+        payload = {"email":"bob@example.org", "password":"password"}
+        res = self.client.post("/accounts/login/", payload, content = "application/json")
+        self.assertRedirects(res, "/checks/")
 
     ### Login with a bad token and check that it redirects
+    def test_login_with_bad_token_redirects(self):
+        # payload = {"username":"bob@example.org", "token":"password"}
+        # res = self.client.post("/accounts/login/", payload, content = "application/json")
+        # self.assertRedirects(res, "/checks/")
+
+        r = self.client.post("/accounts/check_token/alice/secret-tokken/")
+        self.assertRedirects(r, "/accounts/login/")
+
 
     ### Any other tests?
--- hc/accounts/views.py
+++ hc/accounts/views.py
@@ -53,6 +53,7 @@ def _associate_demo_check(request, user):
 
 
 def login(request):
+    #import pdb; pdb.set_trace()
     bad_credentials = False
     if request.method == 'POST':
         form = EmailPasswordForm(request.POST)
