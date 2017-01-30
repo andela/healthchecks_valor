8ad5f8f2a096ec006c5f8c3a9e5b405f3734a85e
andela-ik <ian.kingori@andela.com>
2017-01-30 20:20:43 +0300

    [chore #137591921]: Fix Admin Test

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -1,5 +1,6 @@
 from hc.api.models import Channel, Check
 from hc.test import BaseTestCase
+from django.contrib.auth.models import User
 
 
 class ApiAdminTestCase(BaseTestCase):
@@ -7,13 +8,17 @@ class ApiAdminTestCase(BaseTestCase):
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
-
         ### Set Alice to be staff and superuser and save her :)
+        """" Set Alice as Admin and staff"""
+        self.alice.is_staff = True
+        self.alice.is_admin = True
+        self.alice.save()
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
-
         ch = Channel(user=self.alice, kind="pushbullet", value="test-token")
         ch.save()
 
         ### Assert for the push bullet
+        """ Push bullet"""
+        self.assertEqual(ch.kind,"pushbullet")
--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -12,7 +12,10 @@ class CheckModelTestCase(TestCase):
 
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
+
         ### Repeat above test for when check is an empty string
+        check.tags = ""
+        self.assertEquals(check.tags_list(), [])
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -35,4 +38,8 @@ class CheckModelTestCase(TestCase):
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
 
-    ### Test that when a new check is created, it is not in the grace period
+        ### Test that when a new check is created, it is not in the grace period
+        """ Assert on creation of a new check, it is not in grace period"""
+        check1 = Check()
+        check1.save()
+        self.assertFalse(check1.in_grace_period())
