a3f069974c92e5ab577da03ab42fbb1c4429d873
andela-hms <humphrey.musonye@andela.com>
2017-01-30 12:47:34 +0300

    [chore #137591919] Fixed add_check tests

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -1,8 +1,14 @@
-from hc.api.models import Check
+from hc.api.models import Check, Channel
 from hc.test import BaseTestCase
 
 
 class AddCheckTestCase(BaseTestCase):
+    
+    def setUp(self):
+        super(AddCheckTestCase, self).setUp()
+        self.channel = Channel(user=self.alice, kind="email")
+        self.channel.value = "alice@example.org"
+        self.channel.save()
 
     def test_it_works(self):
         url = "/checks/add/"
@@ -12,3 +18,11 @@ class AddCheckTestCase(BaseTestCase):
         assert Check.objects.count() == 1
 
     ### Test that team access works
+    def test_team_access_works(self):
+        url = "/checks/add/"
+        self.client.login(username="bob@example.org", password="password")
+        self.client.post(url)
+
+        check = Check.objects.get()
+        # Bob is assigned all channels that belong to Alice
+        self.assertEqual(check.user, self.alice)
