872aee20405f3ef6ac50a0f92278f30e602f3e9b
andela-hms <humphrey.musonye@andela.com>
2017-02-22 09:02:11 +0300

    [Feature #137591927] Used GET method to find  url status code

--- hc/front/tests/test_unresolved.py
+++ hc/front/tests/test_unresolved.py
@@ -5,5 +5,5 @@ class Unresolved(BaseTestCase):
     def test_it_works(self):
         url = "/unresolved/"
         self.client.login(username="alice@example.org", password="password")
-        res = self.client.post(url)
+        res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
